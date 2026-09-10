a9880474a3ac8427aa54046fb3907c3a277654c2
lunix <lunix@mirkx.org>
2026-09-10 12:37:15 +0200

    chore: bump versions
    - Python package: 0.2.0 -> 0.3.0
    - Web tests package: 1.0.0 -> 1.1.0
    - Userscript: 2026-06-01 -> 2026-09-10

--- userscript/add-dccbot-btn.js
+++ userscript/add-dccbot-btn.js
@@ -2,7 +2,7 @@
 // @name         add-dccbot-btn
 // @namespace    https://github.com/luni/dccbot/
 // @website      https://github.com/luni/dccbot/
-// @version      2026-06-01
+// @version      2026-09-10
 // @description  Add button for DCCbot to automate downloads.
 // @author       luni
 // @match        https://www.xdcc.eu/search.php*
